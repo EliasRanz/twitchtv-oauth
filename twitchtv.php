ed3b7f224ecf6a04b9eb47c26e7b2d16dd8124b1
Elias Ranz-Schleifer <josefranz@gmail.com>
2013-07-05 14:41:13 -0400

    Commented Code
    Just added a few comments and removed unneeded function. If you have questions about the update please let me know.

--- twitchtv.php
+++ twitchtv.php
@@ -1,11 +1,20 @@
 <?php
+/*
+ * TwitchTV API code by Elias Ranz-Schleifer
+ * Thank you for using my code please refer to
+ * https://github.com/Xxplosions/twitchtv-oauth for future updates
+ * Have questions? Contact Elias on Twitter (https://twitter.com/xxplosions)
+ * Check out my livestream at http://twitch.tv/xxplosions (It would be amazing to chat with you about future updates)
+ */
+ 
 class TwitchTV {
   var $base_url = "https://api.twitch.tv/kraken/";
-  var $client_id = 'INSERT CLIENT ID HERE';
-  var $client_secret = "INSERT CLIENT SECRET HERE";
-  var $redirect_url = 'INSERT REDIRECT URL HERE';
-	var $scope_array = array('user_read','channel_read','chat_login','user_follows_edit','channel_editor','channel_commercial');
+  var $client_id = 'INSERT CLIENT ID HERE'; //change this value, should be your TwitchTV Application Client ID
+  var $client_secret = "INSERT CLIENT SECRET HERE"; //change this value, should be your TwitchTV Application Client Secret 
+  var $redirect_url = 'INSERT REDIRECT URL HERE'; //change this value, should be your TwitchTV Application Rerdirect URL
+  var $scope_array = array('user_read','channel_read','chat_login','user_follows_edit','channel_editor','channel_commercial');
 	
+	//generates the url based on the scopes that have been given.
 	public function authenticate() {	
 		$i = 0;
 		$return = '';
@@ -28,6 +37,7 @@ class TwitchTV {
 		return $authenticate_url;
 	}
 	
+	//Get the Access Token, expects the code value returned from the TwitchTV callback
 	function get_access_token($code) {
 		$ch = curl_init($this->base_url . "oauth2/token");
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
@@ -46,6 +56,7 @@ class TwitchTV {
 		return $response["access_token"];
 	}
 	
+	//Gets the username based on an access token. This will let you know who's Authenticated, expects Access Token
 	function authenticated_user($access_token) {
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $this->base_url . "user");
@@ -67,13 +78,13 @@ class TwitchTV {
 		
 	}
 	
-	// used to send the user to the authentication page for twitch once done we can use their channel data
+	// CHecks to see if the user exists on TwitchTV, if it doesn't exist then it will return false, expects a TwitchTV username
 	public function validate_stream($username) {
 		$curl = curl_init();
 		curl_setopt_array($curl, array(
-										CURLOPT_RETURNTRANSFER => 1,
-										CURLOPT_URL => $this->base_url.'users/'.$username
-									  )
+						CURLOPT_RETURNTRANSFER => 1,
+						CURLOPT_URL => $this->base_url.'users/'.$username
+					      )
 		);
 		
 		$result = curl_exec($curl);
@@ -91,12 +102,13 @@ class TwitchTV {
 		}
 	}
 	
+	//Loads in the Display Name, Stream Status, and Banner, expects TwitchTV username
 	public function load_channel($channel) {
 		//initiate connection to the twitch.tv servers
 		$curl = curl_init();
 		curl_setopt_array($curl, array( CURLOPT_RETURNTRANSFER => 1,
-										CURLOPT_URL => $this->base_url . 'channels/'. $channel .'?client_id=' . $this->client_id
-									  )
+					CURLOPT_URL => $this->base_url . 'channels/'. $channel .'?client_id=' . $this->client_id
+				 )
 		);
 		$result = curl_exec($curl);
 		//makes sure that the cURL was excuted if not it generates the error stating that it didn't succeed.
@@ -107,21 +119,23 @@ class TwitchTV {
 			if(!empty($channel)) {
 				$return = json_decode($result);
 				$stream_details = array('display_name' => $return->display_name,
-											  'status' => $return->status,
-											    'chat' => $return->_links->chat,
-											  'banner' => $return->banner);
+							'status' => $return->status,
+							'chat' => $return->_links->chat,
+							'banner' => $return->banner);
 				return $stream_details;
 			}
 		}
 		curl_close($curl);
 	}
 	
+	//Returns the Stream Title based on what the user currently has on their stream, expects TwitchTV username.
 	public function get_stream_title($username) {
 		$channel_data = json_decode(file_get_contents($this->base_url.'channels/'.$username));
 		$title = $channel_data->status;
 		return $title;
 	}
 	
+	//Updates a the Authenticated users stream title, expects Access Token, optional values are Stream Title and Stream Game
 	public function update_stream_title($access_token,$title = null,$game = null) {
 		$username = $this->authenticated_user($access_token);
 		if($username != 'Unauthorized') {
@@ -155,12 +169,13 @@ class TwitchTV {
 		}
 	}
 	
+	//This loads the viewer count for a stream, expects TwitchTV channel.
 	public function load_stream_stats($channel) {
 		//initiate connection to the twitch.tv servers
 		$curl = curl_init();
 		curl_setopt_array($curl, array( CURLOPT_RETURNTRANSFER => 1,
-										CURLOPT_URL => $this->base_url . 'streams/'. $channel .'?client_id=' . $this->client_id
-									  )
+				        	CURLOPT_URL => $this->base_url . 'streams/'. $channel .'?client_id=' . $this->client_id
+				 )
 		);
 		$result = curl_exec($curl);
 		//makes sure that the cURL was excuted if not it generates the error stating that it didn't succeed.
@@ -182,36 +197,8 @@ class TwitchTV {
 		curl_close($curl);
 	}
 	
-	public function stream_status($channel) {
-		if($channel) {
-		//initiate connection to the twitch.tv servers
-		$curl = curl_init();
-		curl_setopt_array($curl, array( CURLOPT_RETURNTRANSFER => 1,
-										CURLOPT_URL => $this->base_url . 'streams/'. $channel .'?client_id=' . $this->client_id
-									  )
-		);
-		$result = curl_exec($curl);
-		//makes sure that the cURL was excuted if not it generates the error stating that it didn't succeed.
-		if(!curl_exec($curl)){
-		    die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
-		} else {
-			//cURL Response worked
-			if(!empty($channel)) {
-				$return = json_decode($result);
-				if($return->stream == null) {
-					$offline = "Stream Offline";
-					return $offline;
-				} else {
-					$online = "Stream Online";
-					return $online;
-				}
-			}
-		}
-		curl_close($curl);
-		} else {
-			return;
-		}
-	}
+
+
 	
 	public function follower_count($channel) {
 		//initiate connection to the twitch.tv servers
@@ -235,31 +222,35 @@ class TwitchTV {
 		curl_close($curl);
 	}
 	
+	//Loads the Online or Offline status of a stream, expect TwitchTV Username
 	public function stream_online_status($channel) {
 		//initiate connection to the twitch.tv servers
 		$curl = curl_init();
 		curl_setopt_array($curl, array( CURLOPT_RETURNTRANSFER => 1,
-										CURLOPT_URL => $this->base_url . 'streams/'. $channel .'?client_id=' . $this->client_id
-									  )
+						CURLOPT_URL => $this->base_url . 'streams/'. $channel .'?client_id=' . $this->client_id
+				              )
 		);
-		$result = curl_exec($curl);
+		$result = json_decode(curl_exec($curl));
+		
 		//makes sure that the cURL was excuted if not it generates the error stating that it didn't succeed.
 		if(!curl_exec($curl)){
 		    die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
 		} else {
 			//cURL Response worked
 			if(!empty($channel)) {
-				$return = json_decode($result);
-				if($return->stream == null) {
+				if($result->stream == null) {
 					return false;
 				} else {
 					return true;
 				}
+			} else {
+				echo "shit";
 			}
 		}
 		curl_close($curl);
 	}
 	
+	//Loads the streams video, expects TwitchTV Username, additional paramaters allow for customizing the height and width of the embed
 	public function load_video($channel,$height = null, $width = null) {
 		//defaults for stream embed dimensions, set so you can pass in the height and width outside of this function
 		if($height == null && $width == null) {
@@ -269,7 +260,6 @@ class TwitchTV {
 		//make sure that a channel is passed in so that it doesn't return an invalid embed code
 		if(!empty($channel)) {
 			//embed code for the video thanks to twitch.tv
-			//$embed_code = '<iframe width="'.$width.'" height="'.$height.'" src="http://www.twitch.tv/widgets/live_embed_player.swf?channel='.$channel.'&auto_play=true&start_volume=25" frameborder="0" allowfullscreen="true" auto_play="true" start_volume="25"></iframe>';
 			$embed_code = '<object type="application/x-shockwave-flash" height="'.$height.'" width="'.$width.'" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel='.$channel.'" bgcolor="#000000">
 								<param name="allowFullScreen" value="true" />
 								<param name="allowScriptAccess" value="always" />
@@ -283,6 +273,7 @@ class TwitchTV {
 		}
 	}
 	
+	//Loads the streams chat, expects TwitchTV Username, additional paramaters allow for customizing the height and width of the embed
 	public function load_chat($channel,$height = null, $width = null) {
 		//defaults for stream embed dimensions, set so you can pass in the height and width outside of this function
 		if($height == null && $width == null) {
@@ -298,6 +289,7 @@ class TwitchTV {
 		}
 	}
 	
+	//Gets the current directory of all the games on TwitchTV currently streaming. This is returned in JSON to allow for an autocomplete functionality
 	public function get_games() {
 		$game = array();
 		for($i = 0; $i < 5; $i++){
@@ -316,6 +308,8 @@ class TwitchTV {
 		return json_encode($games);
 	}
 	
+	//Call this function if you want to run a commercial on the stream. Expects a access token, but can take a time
+	//for information about the length value please see https://github.com/justintv/Twitch-API/blob/master/v2_resources/channels.md#post-channelschannelcommercial
 	public function run_commercial($access_token, $length = 30) {
 		$username = $this->authenticated_user($access_token);
 		$ch = curl_init($this->base_url . "channels/".$username.'/commercial');
